5b9181665255f7d58bbcc0ab98e1fbbeb8e69f13
Dimitri-Igoshev <d.o.igoshev@gmail.com>
2023-04-07 22:20:54 +0200

    Rating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
+import { useEffect, useState } from 'react';
+import { Button, HTag, Rating } from '@/components';
+
 export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(3);
   return (
-    <></>
+    <>
+      <Rating rating={rating} isEditable setRating={setRating} />
+    </>
   );
 }
